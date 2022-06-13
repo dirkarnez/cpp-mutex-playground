79cea43f0d7f7471ac2a63bec6771304a7201475
dirkarnez <smalldirkalex@gmail.com>
2022-06-13 22:31:32 +0800

    - revamp, wip

--- local-build.cmd
+++ local-build.cmd
@@ -10,8 +10,8 @@ cd /d %~dp0
 @REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
 set PATH=^
-D:\Softwares\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\mingw64;^
-D:\Softwares\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
 D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
 cmake.exe -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -B./build &&^
